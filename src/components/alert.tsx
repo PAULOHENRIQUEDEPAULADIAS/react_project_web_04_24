2740b1b116a578b37f4c4dd9577ca7cb9a6eeb52
Paulo Henrique de Paula Dias <141594243+PAULOHENRIQUEDEPAULADIAS@users.noreply.github.com>
2024-11-21 13:03:42 -0300

    PIP - Regulating Alert

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,11 +1,10 @@
 
 
+alert("Este alert esta em funcionamento!")
+
 export default function Alert(){
     return (
         <div className="alert">
-            <div className="alert-icon">
-                alert("Este alert esta em funcionamento!")
-            </div>
         </div>
     )
 }
