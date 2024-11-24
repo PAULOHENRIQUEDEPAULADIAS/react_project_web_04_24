fa6da7ece3815f2b7549f90762f3f51fdfb8bbc4
Paulo Henrique de Paula Dias <141594243+PAULOHENRIQUEDEPAULADIAS@users.noreply.github.com>
2024-11-24 18:12:10 -0300

    PIP - Datepicker solved in index.js

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,7 +4,7 @@ export { default as Box } from './box';
 export { default as Container } from './container';
 export { default as Card } from './card';
 export { default as Checkbox } from './checkbox';
-export { default as DatePicker } from './datePicker';
+export { default as DatePicker } from './datepicker';
 export { default as DateTimePicker } from './dateTimePicker';
 export { default as Fab } from './fab';
 export { default as Grid } from './grid';
